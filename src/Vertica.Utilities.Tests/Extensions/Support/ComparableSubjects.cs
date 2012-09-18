46ddba24a858a74ccc2c9cc766013beace6747ee
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2012-09-18 19:41:14 +0200

    added pre-requisite extensions for IComparable<>

---
+++ src/Vertica.Utilities.Tests/Extensions/Support/ComparableSubjects.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Vertica.Utilities.Tests.Extensions.Support
+{
+	internal class ComparableSubject : IComparable
+	{
+		private readonly IComparable _inner;
+
+		public ComparableSubject(IComparable inner)
+		{
+			_inner = inner;
+		}
+
+		public int CompareTo(object obj)
+		{
+			return _inner.CompareTo(obj);
+		}
+	}
+
+	internal class GenericComparableSubject<T> : IComparable<GenericComparableSubject<T>> where T : IComparable<T>
+	{
+		private readonly T _inner;
+
+		public GenericComparableSubject(T inner)
+		{
+			_inner = inner;
+		}
+
+		public int CompareTo(GenericComparableSubject<T> other)
+		{
+			return _inner.CompareTo(other._inner);
+		}
+	}
+}
---
+++ src/Vertica.Utilities/Extensions/Comparable.Extensions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vertica.Utilities.Extensions.ComparableExt
+{
+	public static class ComparableExtensions
+	{
+		#region IComparable<>
+
+		public static bool IsEqualTo<T>(this T first, T second) where T : IComparable<T>
+		{
+			return first.CompareTo(second) == 0;
+		}
+
+		public static bool IsEqualTo<T>(this T first, T second, IComparer<T> comparer)
+		{
+			return comparer.Compare(first, second) == 0;
+		}
+
+		public static bool IsDifferentFrom<T>(this T first, T second) where T : IComparable<T>
+		{
+			return !IsEqualTo(first, second);
+		}
+
+		public static bool IsDifferentFrom<T>(this T first, T second, IComparer<T> comparer)
+		{
+			return !IsEqualTo(first, second, comparer);
+		}
+
+		public static bool IsAtMost<T>(this T first, T second) where T : IComparable<T>
+		{
+			return first.CompareTo(second) <= 0;
+		}
+
+		public static bool IsAtMost<T>(this T first, T second, IComparer<T> comparer)
+		{
+			return comparer.Compare(first, second) <= 0;
+		}
+
+		public static bool IsAtLeast<T>(this T first, T second) where T : IComparable<T>
+		{
+			return first.CompareTo(second) >= 0;
+		}
+
+		public static bool IsAtLeast<T>(this T first, T second, IComparer<T> comparer)
+		{
+			return comparer.Compare(first, second) >= 0;
+		}
+
+		public static bool IsLessThan<T>(this T first, T second) where T : IComparable<T>
+		{
+			return first.CompareTo(second) < 0;
+		}
+
+		public static bool IsLessThan<T>(this T first, T second, IComparer<T> comparer)
+		{
+			return comparer.Compare(first, second) < 0;
+		}
+
+		public static bool IsMoreThan<T>(this T first, T second) where T : IComparable<T>
+		{
+			return first.CompareTo(second) > 0;
+		}
+
+		public static bool IsMoreThan<T>(this T first, T second, IComparer<T> comparer)
+		{
+			return comparer.Compare(first, second) > 0;
+		}
+
+		#endregion
+
+		#region IComparable
+
+		public static bool IsEqualTo(this IComparable first, object second)
+		{
+			return first.CompareTo(second) == 0;
+		}
+
+		public static bool IsDifferentFrom(this IComparable first, object second)
+		{
+			return !IsEqualTo(first, second);
+		}
+
+		public static bool IsAtMost(this IComparable first, object second)
+		{
+			return first.CompareTo(second) <= 0;
+		}
+
+		public static bool IsAtLeast(this IComparable first, object second)
+		{
+			return first.CompareTo(second) >= 0;
+		}
+
+		public static bool IsLessThan(this IComparable first, object second)
+		{
+			return first.CompareTo(second) < 0;
+		}
+
+		public static bool IsMoreThan(this IComparable first, object second)
+		{
+			return first.CompareTo(second) > 0;
+		}
+
+		#endregion
+
+		// TODO: reverse comparer
+		/*public static IComparer<T> Reverse<T>(this IComparer<T> comparer)
+		{
+			Guard.AgainstNullArgument("comparer", comparer);
+			return new ReversedComparer<T>(comparer);
+		}*/
+	}
+}
