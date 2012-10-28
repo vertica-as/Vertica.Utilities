9eea071ea0147fa1234fcbafb10a9c83cdb26037
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2012-10-28 12:46:54 +0100

    added pre-requisites Name.Of() and Value.Of()

---
+++ src/Vertica.Utilities/Reflection/NameAndValue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Vertica.Utilities.Reflection
+{
+	public static class Name
+	{
+		public static string Of<T>(Expression<Func<T>> argumentExpression)
+		{
+			var memberExp = (MemberExpression)argumentExpression.Body;
+			return memberExp.Member.Name;
+		}
+
+		public static string Of<T, U>(Expression<Func<T, U>> argumentExpression)
+		{
+			var memberExp = (MemberExpression)argumentExpression.Body;
+			return memberExp.Member.Name;
+		}
+	}
+
+	public static class Value
+	{
+		public static T Of<T>(Expression<Func<T>> argumentExpression)
+		{
+			Func<T> func = argumentExpression.Compile();
+			return func();
+		}
+	}
+}
