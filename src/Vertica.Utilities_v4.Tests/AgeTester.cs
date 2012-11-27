cabf4b58674100e575e91bc2a66210f7da7a4afc
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2012-11-27 13:29:20 +0100

    added constructor and properties

---
+++ src/Vertica.Utilities_v4.Tests/AgeTester.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NUnit.Framework;
+using Testing.Commons;
+using Testing.Commons.Time;
+using Vertica.Utilities_v4.Extensions.TimeExt;
+using Vertica.Utilities_v4.Testing;
+using Vertica.Utilities_v4.Tests.Support;
+
+namespace Vertica.Utilities_v4.Tests
+{
+	[TestFixture]
+	public class AgeTester
+	{
+		private static readonly DateTime _keyDateInHistory = new DateTime(1977, 3, 11);
+
+		#region construction
+
+		[Test]
+		public void AdventConstructor_OneDayAgo_PropertiesSetTo1DayAgo()
+		{
+			DateTime now = _keyDateInHistory;
+			using (TimeReseter.Set(now))
+			{
+				DateTime yesterday = Time.Yesterday.DateTime;
+				var subject = new Age(yesterday);
+
+				Assert.That(subject, Must.Be.Age()
+					.WithBounds(advent: yesterday, terminus: now)
+					.Elapsed(1.	Days())
+					.WithComponents(days: 1, weeks: 0, months: 0, years: 0));
+			}
+		}
+
+		[Test]
+		public void AdventConstructor_OneWeekAgo_PropertiesSetTo1WeekAgo()
+		{
+			DateTime now = _keyDateInHistory;
+			using (TimeReseter.Set(now))
+			{
+				DateTime oneWeekAgo = now.AddDays(-7);
+				var subject = new Age(oneWeekAgo);
+
+				Assert.That(subject, Must.Be.Age()
+					.WithBounds(advent: oneWeekAgo, terminus: now)
+					.Elapsed(1.Weeks())
+					.WithComponents(days: 0, weeks: 1, months: 0, years: 0));
+			}
+		}
+
+		[Test]
+		public void AdventConstructor_TwoMonthsThreeDaysAgo_PropertiesSet()
+		{
+			DateTime now = _keyDateInHistory;
+			using (TimeReseter.Set(now))
+			{
+				DateTime twoMonthsThreeDaysAgo = now.AddMonths(-2).AddDays(-3);
+				var subject = new Age(twoMonthsThreeDaysAgo);
+				Assert.That(subject, Must.Be.Age()
+					.WithBounds(twoMonthsThreeDaysAgo, now)
+					.Elapsed(now - twoMonthsThreeDaysAgo)
+					.WithComponents(days: 3, weeks: 0, months: 2, years: 0));
+			}
+		}
+
+		[Test]
+		public void AdventConstructor_OneWeekAgo_SetsTimeProperties()
+		{
+			DateTime now = _keyDateInHistory;
+			using (TimeReseter.Set(now))
+			{
+				var subject = new Age(1.Weeks().Ago().DateTime);
+
+				Assert.That(subject.Years, Is.EqualTo(0));
+				Assert.That(subject.Months, Is.EqualTo(0));
+				Assert.That(subject.Weeks, Is.EqualTo(1));
+				Assert.That(subject.Days, Is.EqualTo(0));
+			}
+		}
+
+		#endregion
+
+	}
+}
---
+++ src/Vertica.Utilities_v4/Age.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace Vertica.Utilities_v4
+{
+	/* based on: http://www.singular.co.nz/blog/archive/2007/05/01/building-an-age-class-in-csharp.aspx */
+	public struct Age
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Creates a new instance of Age starting from the given date until now
+		/// </summary>
+		/// <param name="advent"></param>
+		public Age(DateTime advent)
+			: this(advent, getNowFromKind(advent))
+		{
+		}
+
+		/// <summary>
+		/// Creates a new instance of Age starting from the given date until now
+		/// </summary>
+		/// <param name="advent"></param>
+		/// <param name="terminus"></param>
+		// Implementation based on http://tommycarlier.blogspot.com/2006/02/years-months-and-days-between-2-dates.html#links
+		public Age(DateTime advent, DateTime terminus)
+		{
+			_advent = advent;
+			_terminus = terminus;
+
+			// only extracts down to 'days', so the TimeOfDay needs to be 
+			// normalised for this calculation
+			TimeSpan time = advent.TimeOfDay;
+			if (time > TimeSpan.Zero)
+			{
+				advent = advent.Subtract(time);
+				terminus = terminus.Subtract(time);
+			}
+
+			_years = terminus.Year - advent.Year;
+			_months = terminus.Month - advent.Month;
+			_days = terminus.Day - advent.Day;
+			_weeks = 0;
+
+			if (_days < 0) _months -= 1;
+			while (_months < 0)
+			{
+				_months += 12;
+				_years -= 1;
+			}
+
+			TimeSpan span = terminus - advent.AddYears(_years).AddMonths(_months);
+			_days = (int)Math.Floor(span.TotalDays);
+
+			if (_days > 0)
+			{
+				_weeks = _days / 7;
+				_days = _days % 7;
+			}
+		}
+
+		/// <summary>
+		/// Gets the current date/time for universal vrs local time
+		/// </summary>
+		/// <param name="time"></param>
+		/// <returns></returns>
+		private static DateTime getNowFromKind(DateTime time)
+		{
+			return time.Kind == DateTimeKind.Utc ? Time.UtcNow.DateTime : Time.Now.DateTime;
+		}
+
+		#endregion
+
+		#region properties
+
+		private readonly int _years;
+		/// <summary>
+		/// Gets the number of years old
+		/// </summary>
+		public int Years
+		{
+			get { return _years; }
+		}
+
+		private readonly int _months;
+		/// <summary>
+		/// Gets the number of months old
+		/// </summary>
+		public int Months
+		{
+			get { return _months; }
+		}
+
+		private readonly int _weeks;
+		/// <summary>
+		/// Gets the number of weeks old
+		/// </summary>
+		public int Weeks
+		{
+			get { return _weeks; }
+		}
+
+		private readonly int _days;
+		/// <summary>
+		/// Gets the number of days old
+		/// </summary>
+		public int Days
+		{
+			get { return _days; }
+		}
+
+		private readonly DateTime _advent;
+		/// <summary>
+		/// Gets/Sets the start of the age
+		/// </summary>
+		public DateTime Advent
+		{
+			get { return _advent; }
+		}
+
+		private readonly DateTime _terminus;
+		/// <summary>
+		/// Gets/Sets the end of the age
+		/// </summary>
+		public DateTime Terminus
+		{
+			get { return _terminus; }
+		}
+
+		/// <summary>
+		/// Gets the elapsed time
+		/// </summary>
+		public TimeSpan Elapsed
+		{
+			get { return new TimeSpan(_terminus.Ticks - _advent.Ticks); }
+		}
+
+		/// <summary>
+		/// Gets a value indicating that the age is empty (both advent and terminus dates are MinValue)
+		/// </summary>
+		/// <returns></returns>
+		public bool IsEmpty
+		{
+			get
+			{
+				return _advent == DateTime.MinValue
+					   && _terminus == DateTime.MinValue;
+			}
+		}
+
+		#endregion
+	}
+}
