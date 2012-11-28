b85a852154dc550e6aae08f904025aa2cd3c93ac
Daniel González García <danielgonnet@gmail.com>
2012-11-28 10:09:27 +0100

    added enum support: Enumeration (previously known as EnumHelper) and IsEnum method

---
+++ src/Vertica.Utilities_v4.Tests/EnumerationTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Vertica.Utilities_v4.Tests
+{
+	[TestFixture]
+	public class EnumerationTester
+	{
+		[Test]
+		public void IsEnum_EnumType_True()
+		{
+			Assert.That(Enumeration.IsEnum<PlatformID>(), Is.True);
+			Assert.That(Enumeration.IsEnum(typeof(PlatformID)), Is.True);
+		}
+	}
+}
---
+++ src/Vertica.Utilities_v4/Enumeration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Vertica.Utilities_v4
+{
+	//* 
+	//* Based upon http://rodenbaugh.net/archive/2006/11/01/Enum-Helper-Class-Using-Generics.aspx
+	//* 
+	public static class Enumeration
+	{
+		public static bool IsEnum<TEnum>() where TEnum : struct, IComparable, IFormattable, IConvertible
+		{
+			return IsEnum(typeof(TEnum));
+		}
+
+		public static bool IsEnum(Type type)
+		{
+			return type.IsEnum;
+		}
+	}
+}
