5de68f6ef35363a94e172abbfa03a4461907f2b1
Daniel González García <danielgonnet@gmail.com>
2012-12-03 10:37:09 +0100

    tested Raise chained event

--- src/Vertica.Utilities_v4.Tests/Eventing/EventHelperTester.cs
+++ src/Vertica.Utilities_v4.Tests/Eventing/EventHelperTester.cs
@@ -53,6 +53,17 @@ namespace Vertica.Utilities_v4.Tests.Eventing
 
 		public event EventHandler<ValueEventArgs<string>> NoAccessorEvent;
 
+		private static event ChainedEventHandler<ChainedEventArgs> _chained;
+
+		internal class EventChainSubject
+		{
+			internal event ChainedEventHandler<ChainedEventArgs> Event;
+			internal bool OnChainedEvent(ChainedEventArgs args)
+			{
+				return Event.Raise(this, args);
+			}
+		}
+
 		#endregion
 
 		#region Raise
@@ -252,6 +263,76 @@ namespace Vertica.Utilities_v4.Tests.Eventing
 			subject.I = 2;
 
 			Assert.That(propertyChangingName, Is.EqualTo("S"));
+		}
+
+		#endregion
+
+		#region chains
+
+		[Test]
+		public void Raise_OwnEvent_HandledBySecond_NoFurtherCallbackExecuted()
+		{
+			string lastHandler = string.Empty;
+			int numberOfHandlersExecuted = 0;
+
+			_chained += (handler, e) =>
+			{ 
+				numberOfHandlersExecuted++;
+				lastHandler = "one";
+			};
+			_chained += (handler, e) =>
+			{
+				numberOfHandlersExecuted++;
+				lastHandler = "two";
+				e.Handled = true;
+			};
+			_chained += (handler, e) =>
+			{
+				numberOfHandlersExecuted++;
+				lastHandler = "three";
+			};
+
+			var args = new ChainedEventArgs();
+
+			bool result = _chained.Raise(this, args);
+
+			Assert.That(result, Is.True);
+			Assert.That(args.Handled, Is.True);
+			Assert.That(numberOfHandlersExecuted, Is.EqualTo(2));
+			Assert.That(lastHandler, Is.EqualTo("two"));
+		}
+
+		[Test]
+		public void Raise_ExternalClassEvent_HandledBySecond_NoFurtherCallbackExecuted()
+		{
+			int numberOfHandlersExecuted = 0;
+
+			var subject = new EventChainSubject();
+			subject.Event += (sender, e) => numberOfHandlersExecuted++;
+			subject.Event += (sender, e) =>
+			{
+				numberOfHandlersExecuted++;
+				e.Handled = true;
+			};
+			subject.Event += (sender, e) => numberOfHandlersExecuted++;
+
+			var args = new ChainedEventArgs();
+			bool result = subject.OnChainedEvent(args);
+
+			Assert.That(result, Is.True);
+			Assert.That(args.Handled, Is.True);
+			Assert.That(numberOfHandlersExecuted, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Raise_EmptyRegistratinList_NoHandling()
+		{
+			var subject = new EventChainSubject();
+			var args = new ChainedEventArgs();
+			bool result = subject.OnChainedEvent(args);
+
+			Assert.That(result, Is.False);
+			Assert.That(args.Handled, Is.False);
 		}
 
 		#endregion
