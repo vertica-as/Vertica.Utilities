fedd3abb04439ef062f61a6fba5ec834c1c00986
Daniel González García <danielgonnet@gmail.com>
2012-12-03 11:31:22 +0100

    took advantage of interfaces

--- src/Vertica.Utilities_v4/Eventing/TypedEventHandler.cs
+++ src/Vertica.Utilities_v4/Eventing/TypedEventHandler.cs
@@ -4,7 +4,7 @@ namespace Vertica.Utilities_v4.Eventing
 {
 	public delegate void EventHandler<in TEventArgs, in TSender>(TSender sender, TEventArgs e) where TEventArgs : EventArgs;
 
-	public delegate void ChainedEventHandler<in T>(object sender, T e) where T : ChainedEventArgs;
+	public delegate void ChainedEventHandler<in T>(object sender, T e) where T : IChainedEventArgs;
 
 	public delegate K ChainedEventHandler<in T, out K>(object sender, T e) where T : IMutableValueEventArgs<K>;
 }
