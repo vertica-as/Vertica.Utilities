3c554c57b82841c32d893562a930c1fabe86c70f
Daniel González García <danielgonnet@gmail.com>
2012-12-03 12:49:56 +0100

    construction comparing amounts

--- src/Vertica.Utilities_v4.Tests/PercentageTester.cs
+++ src/Vertica.Utilities_v4.Tests/PercentageTester.cs
@@ -19,10 +19,27 @@ namespace Vertica.Utilities_v4.Tests
 		[Test]
 		public void FromFraction_SetsValueAndFraction()
 		{
-			var sixtyPercent = Percentage.FromFraction(.6d);
+			Percentage sixtyPercent = Percentage.FromFraction(.6d);
 
 			Assert.That(sixtyPercent.Value, Is.EqualTo(60d));
 			Assert.That(sixtyPercent.Fraction, Is.EqualTo(.6d));
+		}
+
+		[Test]
+		public void FromAmounts_CalculatesPercentage()
+		{
+			Percentage eightyPercent = Percentage.FromAmounts(60L, 75L);
+
+			Assert.That(eightyPercent.Value, Is.EqualTo(80d));
+			Assert.That(eightyPercent.Fraction, Is.EqualTo(0.8d));
+
+			Percentage tenPercent = Percentage.FromAmounts(10d, 100d);
+			Assert.That(tenPercent.Value, Is.EqualTo(10d));
+			Assert.That(tenPercent.Fraction, Is.EqualTo(0.1d));
+
+			Percentage thousandPercent = Percentage.FromAmounts(100d, 10d);
+			Assert.That(thousandPercent.Value, Is.EqualTo(1000d));
+			Assert.That(thousandPercent.Fraction, Is.EqualTo(10d));
 		}
 
 		#endregion
--- src/Vertica.Utilities_v4/Percentage.cs
+++ src/Vertica.Utilities_v4/Percentage.cs
@@ -7,19 +7,38 @@ namespace Vertica.Utilities_v4
 {
 	public struct Percentage
 	{
-		public Percentage(double value) : this()
-		{
-			Value = value;
-			Fraction = value / 100d;
-		}
-
 		public double Value { get; private set; }
 		public double Fraction { get; private set; }
 
+		#region construction
+
+		public Percentage(double value) : this()
+		{
+			Value = value;
+			Fraction = value/100d;
+		}
+
 		public static Percentage FromFraction(double fraction)
 		{
-			return new Percentage(fraction * 100d);
+			return new Percentage(fraction*100d);
 		}
+
+		internal static readonly string _divideByZeroMessage = new DivideByZeroException().Message;
+
+		public static Percentage FromAmounts(long given, long total)
+		{
+			Guard.AgainstArgument("total", total == 0, _divideByZeroMessage);
+			return new Percentage(given / (double)total * 100d);
+		}
+
+		public static Percentage FromAmounts(double given, double total)
+		{
+			Guard.AgainstArgument("total", total == 0d, _divideByZeroMessage);
+			return new Percentage(given / total * 100d);
+		}
+
+		#endregion
+
 
 	}
 }
