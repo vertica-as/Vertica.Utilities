f802549845c0d1191b83a325eb010ae29194d5d1
Daniel González García <danielgonnet@gmail.com>
2012-12-05 18:02:03 +0100

    added comb generation and guessable ids generation with low index fragmentation

---
+++ src/Vertica.Utilities_v4.Tests/CombTester.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Vertica.Utilities_v4.Tests
+{
+	[TestFixture]
+	public class UniqueIdentifierTester
+	{
+		[Test]
+		public void Guessable_IdsThatGrow()
+		{
+			IComparer<string> comparer = StringComparer.OrdinalIgnoreCase;
+			var combRepresentations = new string[10];
+			for (int i = 0; i < 10; i++)
+			{
+				combRepresentations[i] = UniqueIdentifier.Guessable().ToString("N");
+				if (i > 0)
+				{
+					// compare with previous
+					Assert.That(combRepresentations[i], Is.GreaterThan(combRepresentations[i -1]).Using(comparer));
+				}
+			}
+		}
+	}
+}
