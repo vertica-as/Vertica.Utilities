9732b0f70529b7cefcd6b56af7de0df0e8dabe3e
Daniel González García <danielgonnet@gmail.com>
2012-12-17 16:18:28 +0100

    able to reverse comparers

---
+++ src/Vertica.Utilities_v4.Tests/Comparisons/ReversedComparerTester.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Vertica.Utilities_v4.Comparisons;
+using Vertica.Utilities_v4.Tests.Comparisons.Support;
+
+namespace Vertica.Utilities_v4.Tests.Comparisons
+{
+	[TestFixture]
+	public class ReversedComparerTester
+	{
+		private static readonly IComparer<ComparisonSubject> _toBeReversed =
+			   new ComparisonComparer<ComparisonSubject>((x, y) => x.Property2.CompareTo(y.Property2));
+
+		[Test]
+		public void Ctor_DefaultsToAscending()
+		{
+			var subject = new ReversedComparer<ComparisonSubject>(_toBeReversed);
+			Assert.That(subject.SortDirection, Is.EqualTo(Direction.Ascending));
+		}
+
+		[Test]
+		public void Ctor_SetsDirection()
+		{
+			var subject = new ReversedComparer<ComparisonSubject>(_toBeReversed, Direction.Descending);
+			Assert.That(subject.SortDirection, Is.EqualTo(Direction.Descending));
+		}
+
+		[Test]
+		public void Compare_ComparedTheSelectedProperty_HonoringDirection()
+		{
+			var subject = new ReversedComparer<ComparisonSubject>(
+				_toBeReversed, Direction.Ascending);
+			Assert.That(subject.Compare(ComparisonSubject.One, ComparisonSubject.Two), Is.GreaterThan(0));
+
+			subject = new ReversedComparer<ComparisonSubject>(_toBeReversed, Direction.Descending);
+			Assert.That(subject.Compare(ComparisonSubject.One, ComparisonSubject.Two), Is.LessThan(0));
+		}
+	}
+}
---
+++ src/Vertica.Utilities_v4/Comparisons/ReverseComparer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Vertica.Utilities_v4.Comparisons
+{
+	public class ReversedComparer<T> : ChainableComparer<T>
+	{
+		private readonly IComparer<T> _inner;
+
+		public ReversedComparer(IComparer<T> inner) : this(inner, Direction.Ascending) { }
+
+		public ReversedComparer(IComparer<T> inner, Direction direction) : base(direction)
+		{
+			Guard.AgainstNullArgument("inner", inner);
+			_inner = inner;
+		}
+
+		protected override int DoCompare(T x, T y)
+		{
+			return _inner.Compare(y, x);
+		}
+	}
+}
