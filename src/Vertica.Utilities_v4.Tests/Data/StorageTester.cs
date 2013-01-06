2e498116ea647ed57f56d455d36355b9ea61b806
Daniel González García <danielgonnet@gmail.com>
2013-01-06 10:02:28 +0100

    Storage and tests outside httpcontext

---
+++ src/Vertica.Utilities_v4.Tests/Data/StorageTester.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Vertica.Utilities_v4.Data;
+
+namespace Vertica.Utilities_v4.Tests.Data
+{
+	[TestFixture]
+	public class StorageTester
+	{
+		[Test]
+		public void Can_Save_Data_Outside_HttpContext()
+		{
+			var key = new object();
+			string value = "value";
+			Storage.Data[key] = value;
+
+			Assert.That(Storage.Data[key], Is.SameAs(value));
+		}
+
+		[Test]
+		public void Clear_RemovesAllElements()
+		{
+			Storage.Data.Clear();
+			Storage.Data["one"] = "This is a string";
+			Storage.Data["two"] = 99.9m;
+
+			Assert.That(Storage.Data.Count, Is.EqualTo(2));
+			Storage.Data.Clear();
+			Assert.That(Storage.Data.Count, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void Data_NonExistingKey_Null()
+		{
+			var nonExistingKey = new object();
+			Assert.That(Storage.Data[nonExistingKey], Is.Null);
+		}
+	}
+}
---
+++ src/Vertica.Utilities_v4/Data/Storage.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Web;
+
+namespace Vertica.Utilities_v4.Data
+{
+	/* based on RhinoCommon's LocalData */
+	public static class Storage
+	{
+		private static readonly IStorage _current = new StorageData();
+
+		internal static readonly object LocalStorageKey = new object();
+
+		private class StorageData : IStorage
+		{
+			[ThreadStatic]
+			private static Hashtable _threadHashtable;
+
+			private static Hashtable LocalHashtable
+			{
+				get
+				{
+					if (!RunningInWeb)
+					{
+						return _threadHashtable ??
+						(
+							_threadHashtable = new Hashtable()
+						);
+					}
+					var webHashtable = HttpContext.Current.Items[LocalStorageKey] as Hashtable;
+					if (webHashtable == null)
+					{
+						HttpContext.Current.Items[LocalStorageKey] = webHashtable = new Hashtable();
+					}
+					return webHashtable;
+				}
+			}
+
+			public object this[object key]
+			{
+				get { return LocalHashtable[key]; }
+				set { LocalHashtable[key] = value; }
+			}
+
+			public void Clear()
+			{
+				LocalHashtable.Clear();
+			}
+
+			public int Count
+			{
+				get { return LocalHashtable.Count; }
+			}
+		}
+
+		/// <summary>
+		/// Gets the current data
+		/// </summary>
+		/// <value>The data.</value>
+		public static IStorage Data
+		{
+			get { return _current; }
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether running in the web context
+		/// </summary>
+		/// <value><c>true</c> if [running in web]; otherwise, <c>false</c>.</value>
+		public static bool RunningInWeb
+		{
+			get { return HttpContext.Current != null; }
+		}
+	}
+}
