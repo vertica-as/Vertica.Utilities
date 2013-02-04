5499d5dc782264d495a93e4af32279866ba0a9ee
Daniel González García <danielgonnet@gmail.com>
2013-02-04 22:58:27 +0100

    added Impersonate

---
+++ src/Vertica.Utilities_v4/Security/Impersonate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace Vertica.Utilities_v4.Security
+{
+	public class Impersonate
+	{
+		private readonly IWindowsIdentityProvider _provider;
+
+		internal Impersonate(IWindowsIdentityProvider provider)
+		{
+			_provider = provider;
+		}
+
+		public static Impersonate Using(IWindowsIdentityProvider provider)
+		{
+			return new Impersonate(provider);
+		}
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		static extern bool CloseHandle(IntPtr handle);
+		public void Do(Action action)
+		{
+			using (_provider)
+			{
+				using (WindowsIdentity id = _provider.GetWindowsIdentity())
+				{
+					using (WindowsImpersonationContext wic = id.Impersonate())
+					{
+						try
+						{
+							action();
+						}
+						finally
+						{
+							wic.Undo();
+							CloseHandle(id.Token);
+						}
+					}
+				}
+			}
+		}
+	}
+}
