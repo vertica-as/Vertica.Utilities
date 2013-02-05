dda194497a3e319fc5e8abec8f5c78b9fd6f8268
Daniel González García <danielgonnet@gmail.com>
2013-02-06 00:08:05 +0100

    Added pipes and filters

---
+++ src/Vertica.Utilities_v4/Patterns/PipesAndFilters.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Vertica.Utilities_v4.Extensions.EnumerableExt;
+
+namespace Vertica.Utilities_v4.Patterns
+{
+	/* based on http://nayyeri.net/pipes-and-filters-pattern-in-net */
+	public interface IOperation<T>
+	{
+		IEnumerable<T> Execute(IEnumerable<T> input);
+	}
+
+	public class Pipeline<T>
+	{
+		private readonly List<IOperation<T>> _operations;
+
+		public Pipeline()
+		{
+			_operations = new List<IOperation<T>>();
+		}
+
+		public Pipeline(params IOperation<T>[] operations) : this(operations.AsEnumerable()) { }
+
+		public Pipeline(IEnumerable<IOperation<T>> operations) : this()
+		{
+			operations.ForEach(o => Register(o));
+		}
+
+		public Pipeline<T> Register(IOperation<T> operation)
+		{
+			if (operation != null) _operations.Add(operation);
+			return this;
+		}
+
+		public void Execute()
+		{
+			IEnumerable<T> current = Enumerable.Empty<T>();
+			current = _operations.Aggregate(current, (input, operation) => operation.Execute(input));
+			current.Iterate();
+		}
+	}
+}
