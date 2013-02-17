5540371a36effadb2b8237e612b9699e59194d77
Daniel González García <danielgonnet@gmail.com>
2013-02-17 19:34:48 +0100

    added SmartEnumerable

---
+++ src/Vertica.Utilities_v4.Tests/Collections/Support/MustExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Testing.Commons;
+
+namespace Vertica.Utilities_v4.Tests.Collections.Support
+{
+	internal static partial class MustExtensions
+	{
+		public static SmartEntryConstraint<T> Entry<T>(this Must.BeEntryPoint entry, int index, T value, bool isFirst, bool isLast)
+		{
+			return new SmartEntryConstraint<T>(index, value, isFirst, isLast);
+		}
+	}
+}
