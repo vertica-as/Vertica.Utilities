68c83bfa66e19fc30261805b9784ed976d45e8a2
Daniel González García <danielgonnet@gmail.com>
2013-02-17 16:25:08 +0100

    added KeyValueAdapter

---
+++ src/Vertica.Utilities_v4.Tests/Collections/KeyValueAdapter.cs
@@ -0,0 +1,12 @@
+﻿namespace Vertica.Utilities_v4.Tests.Collections
+{
+	/// <summary>
+	/// Initially to adapt the string indexer from IDictionary and NameValueCollection now adapts any indexer.
+	/// </summary>
+	/// <typeparam name="TKey">Type of the key elements.</typeparam>
+	/// <typeparam name="TValue">Type of the values.</typeparam>
+	public abstract class KeyValueAdapter<TKey, TValue>
+	{
+		public abstract TValue this[TKey key] { get; set; }
+	}
+}
