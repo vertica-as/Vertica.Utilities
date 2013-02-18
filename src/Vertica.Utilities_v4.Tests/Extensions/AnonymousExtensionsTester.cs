4934ab446c80e1667ef05f390ff27f61d252e81b
Daniel González García <danielgonnet@gmail.com>
2013-02-18 22:37:32 +0100

    Added .AsTuples() .AsKeyValuePairs() and .AsDictionary() to convert anonymous objects to collections of tuples, pairs or dictionaries

--- src/Vertica.Utilities_v4.Tests/Extensions/AnonymousExtensionsTester.cs
+++ src/Vertica.Utilities_v4.Tests/Extensions/AnonymousExtensionsTester.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using Testing.Commons;
@@ -16,10 +17,12 @@ namespace Vertica.Utilities_v4.Tests.Extensions
 			return new { P1 = p1, P2 = p2 };
 		}
 
+		#region Cast_ByExamaple
+
 		[Test]
 		public void Cast_ByExample_OnObjects_CanStrongTypeSubsequentUses()
 		{
-			var example = new { P1 = default(string), P2 = default(int) };
+			var example = new {P1 = default(string), P2 = default(int)};
 
 			var typed = returnAnonymous("P1", 2).Cast().ByExample(example);
 
@@ -30,17 +33,141 @@ namespace Vertica.Utilities_v4.Tests.Extensions
 		[Test]
 		public void Cast_ByExample_OnEnumerables_CanStrongTypeSubsequentUses()
 		{
-			var example = new { P1 = default(string), P2 = default(int) };
-			var enumerable = new[] { returnAnonymous("p1_1", 1), returnAnonymous("p1_2", 2) };
+			var example = new {P1 = default(string), P2 = default(int)};
+			var enumerable = new[] {returnAnonymous("p1_1", 1), returnAnonymous("p1_2", 2)};
 
-			var typed = enumerable
-				.Select(a => a.Cast().ByExample(example));
+			var typed = enumerable.Select(a => a.Cast().ByExample(example));
 
 			Assert.That(typed, Has.All.InstanceOf(example.GetType()));
 
-			Assert.That(typed, Must.Be.Constrained(
-				Has.Property("P1").EqualTo("p1_1") & Has.Property("P2").EqualTo(1),
-				Has.Property("P1").EqualTo("p1_2") & Has.Property("P2").EqualTo(2)));
+			Assert.That(typed, Must.Be.Constrained(Has.Property("P1").EqualTo("p1_1") & Has.Property("P2").EqualTo(1), Has.Property("P1").EqualTo("p1_2") & Has.Property("P2").EqualTo(2)));
 		}
+
+		#endregion
+
+
+		#region AsTuples
+
+		[Test]
+		public void AsTuples_TuplesWithPropertyNamesAndValues()
+		{
+			var a = new {P1 = "1", P2 = 2};
+
+			Assert.That(a.AsTuples(), Is.EquivalentTo(new[]
+			{
+				new Tuple<string, object>("P1", "1"),
+				new Tuple<string, object>("P2", 2)
+			}));
+		}
+
+		[Test]
+		public void AsTuples_NullProperties_AreSkipped()
+		{
+			var a = new {P1 = "1", P2 = (string) null, P3 = 0};
+
+			Assert.That(a.AsTuples(), Is.EquivalentTo(new[]
+			{
+				new Tuple<string, object>("P1", "1"),
+				new Tuple<string, object>("P3", 0)
+			}));
+		}
+
+		[Test]
+		public void AsTuples_DefaultValueType_NoValueSkipped()
+		{
+			var a = new {P1 = 1, P2 = default(int), P3 = 3};
+
+			Assert.That(a.AsTuples(), Is.EquivalentTo(new[]
+			{
+				new Tuple<string, object>("P1", 1), 
+				new Tuple<string, object>("P2", 0), 
+				new Tuple<string, object>("P3", 3)
+			}));
+		}
+
+		#endregion
+
+		#region AsKeyValuePairs
+
+		[Test]
+		public void AsKeyValuePairs_PairsWithPropertyNamesAndValues()
+		{
+			var a = new { P1 = "1", P2 = 2 };
+
+			Assert.That(a.AsKeyValuePairs(), Is.EquivalentTo(new[]
+			{
+				new KeyValuePair<string, object>("P1", "1"),
+				new KeyValuePair<string, object>("P2", 2)
+			}));
+		}
+
+		[Test]
+		public void AsAsKeyValuePairs_NullProperties_AreSkipped()
+		{
+			var a = new { P1 = "1", P2 = (string)null, P3 = 0 };
+
+			Assert.That(a.AsKeyValuePairs(), Is.EquivalentTo(new[]
+			{
+				new KeyValuePair<string, object>("P1", "1"),
+				new KeyValuePair<string, object>("P3", 0)
+			}));
+		}
+
+		[Test]
+		public void AsKeyValuePairsAsTuples_DefaultValueType_NoValueSkipped()
+		{
+			var a = new { P1 = 1, P2 = default(int), P3 = 3 };
+
+			Assert.That(a.AsKeyValuePairs(), Is.EquivalentTo(new[]
+			{
+				new KeyValuePair<string, object>("P1", 1), 
+				new KeyValuePair<string, object>("P2", 0), 
+				new KeyValuePair<string, object>("P3", 3)
+			}));
+		}
+
+		#endregion
+
+		#region AsDictionary
+
+		[Test]
+		public void AsDictionary_PairsWithPropertyNamesAndValues()
+		{
+			var a = new { P1 = "1", P2 = 2 };
+			
+			Assert.That(a.AsDictionary(), Is.EquivalentTo(new []
+			{
+				new KeyValuePair<string, object>("P1", "1"),
+				new KeyValuePair<string, object>("P2", 2)
+			}));
+		}
+
+		[Test]
+		public void AsDictionarys_NullProperties_AreSkipped()
+		{
+			var a = new { P1 = "1", P2 = (string)null, P3 = 0 };
+
+			Assert.That(a.AsDictionary(), Is.EquivalentTo(new[]
+			{
+				new KeyValuePair<string, object>("P1", "1"),
+				new KeyValuePair<string, object>("P3", 0)
+			}));
+		}
+
+		[Test]
+		public void AsDictionary_DefaultValueType_NoValueSkipped()
+		{
+			var a = new { P1 = 1, P2 = default(int), P3 = 3 };
+
+			Assert.That(a.AsDictionary(), Is.EquivalentTo(new[]
+			{
+				new KeyValuePair<string, object>("P1", 1), 
+				new KeyValuePair<string, object>("P2", 0), 
+				new KeyValuePair<string, object>("P3", 3)
+			}));
+		}
+
+		#endregion
+
 	}
 }
--- src/Vertica.Utilities_v4/Extensions/Anonnymous.Extensions.cs
+++ src/Vertica.Utilities_v4/Extensions/Anonnymous.Extensions.cs
@@ -1,12 +1,50 @@
-﻿using Vertica.Utilities_v4.Extensions.Infrastructure;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Vertica.Utilities_v4.Extensions.Infrastructure;
 
 namespace Vertica.Utilities_v4.Extensions.AnonymousExt
 {
 	public static class AnonnymousExtensions
 	{
-		public static T ByExample<T>(this CastExtensionPoint<object> obj, T example)
+		public static T ByExample<T>(this CastExtensionPoint<object> obj, T example) where T : class 
 		{
 			return (T)obj.ExtendedValue;
 		}
+
+		public static IEnumerable<Tuple<string, object>> AsTuples<T>(this T anonymousObject) where T : class
+		{
+			return anonymousAs(anonymousObject, Tuple.Create);
+		}
+
+		public static IEnumerable<KeyValuePair<string, object>> AsKeyValuePairs<T>(this T anonymousObject) where T : class
+		{
+			return anonymousAs(anonymousObject, (propName, val) => new KeyValuePair<string, object>(propName, val));
+		}
+
+		public static IDictionary<string, object> AsDictionary<T>(this T anonymousObject) where T : class
+		{
+			return AsKeyValuePairs(anonymousObject)
+				.ToDictionary(p => p.Key, p => p.Value);
+		}
+
+		private static IEnumerable<TResult> anonymousAs<TAnonymous, TResult>(TAnonymous anonymous, Func<string, object, TResult> result) where TAnonymous : class
+		{
+			if (anonymous != null)
+			{
+				PropertyDescriptorCollection props = TypeDescriptor.GetProperties(anonymous);
+				foreach (PropertyDescriptor prop in props)
+				{
+					object val = prop.GetValue(anonymous);
+					if (val != null)
+					{
+						yield return result(prop.Name, val);
+					}
+				}
+			}
+		}
 	}
+
+
 }
