92298de7dde4131cff36ab8155704a9e4df7d2ae
Daniel González García <danielgonnet@gmail.com>
2013-02-28 23:15:35 +0100

    Added ToBoolean()

--- src/Vertica.Utilities_v4.Tests/Extensions/ObjectExtensionsTester.cs
+++ src/Vertica.Utilities_v4.Tests/Extensions/ObjectExtensionsTester.cs
@@ -17,8 +17,46 @@ namespace Vertica.Utilities_v4.Tests.Extensions
 		[Test]
 		public void Safe_NotNullInstance_FunctionExecuted()
 		{
-			Exception notNull = new Exception("msg");
+			var notNull = new Exception("msg");
 			Assert.That(notNull.Safe(o => o.Message), Is.EqualTo("msg"));
 		}
+
+		#region Db
+
+		#region ToBoolean
+
+		[Test]
+		public void ToBoolean_DBNull_null()
+		{
+			object input = DBNull.Value;
+			Assert.That(input.Db().ToBoolean(), Is.Null);
+		}
+
+		[TestCase(true, true)]
+		[TestCase(false, false)]
+		[TestCase("true", true)]
+		[TestCase("FALSE", false)]
+		[TestCase("t", true)]
+		[TestCase("F", false)]
+		[TestCase("0", false)]
+		[TestCase("1", true)]
+		[TestCase("nonBoolean", null)]
+		[TestCase('t', true)]
+		[TestCase('F', false)]
+		[TestCase('0', false)]
+		[TestCase('1', true)]
+		[TestCase('X', null)]
+		[TestCase(1, true)]
+		[TestCase(0, false)]
+		[TestCase(300, null)]
+		[TestCase(3.0, null)]
+		public void ToBoolean_SpecTest(object input, bool? expected)
+		{
+			Assert.That(input.Db().ToBoolean(), Is.EqualTo(expected));
+		}
+
+		#endregion
+
+		#endregion
 	}
 }
--- src/Vertica.Utilities_v4/Extensions/Object.Extensions.cs
+++ src/Vertica.Utilities_v4/Extensions/Object.Extensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Vertica.Utilities_v4.Extensions.Infrastructure;
+using Vertica.Utilities_v4.Extensions.StringExt;
 
 namespace Vertica.Utilities_v4.Extensions.ObjectExt
 {
@@ -19,6 +22,82 @@ namespace Vertica.Utilities_v4.Extensions.ObjectExt
 				result = func(argument);
 			}
 			return result;
+		}
+
+		#region Db, old ADO checking
+
+		public class DbExtensionPoint : ExtensionPoint<object>
+		{
+			public DbExtensionPoint(object value) : base(value) { }
+		}
+
+		public static DbExtensionPoint Db(this object subject)
+		{
+			return new DbExtensionPoint(subject);
+		}
+
+		/// <summary>
+		/// Returns <c>null</c> if the object cannot be converted somehow to bool or if it is<see cref="DBNull.Value"/>.
+		/// </summary>
+		public static bool? ToBoolean(this DbExtensionPoint e)
+		{
+			bool? result = null;
+			try
+			{
+				object o = e.ExtendedValue;
+				if (o != DBNull.Value)
+				{
+					if (o is bool)
+					{
+						result = (bool)o;
+					}
+					else if (IsIntegral(o))
+					{
+						long l = Convert.ToInt64(o);
+						if (l == 0) result = false;
+						if (l == 1) result = true;
+					}
+					else if (o is char)
+					{
+						var c = (char) o;
+						if (c.Equals('0')) result = false;
+						if (c.Equals('1')) result = true;
+						if (c.Equals('t') || c.Equals('T')) result = true;
+						if (c.Equals('f') || c.Equals('F')) result = false;
+					}
+					else
+					{
+						var str = o as string;
+						bool parsed;
+						if (bool.TryParse(str, out parsed))
+						{
+							result = parsed;
+						}
+						else
+						{
+							IEqualityComparer<string> comparer = StringComparer.OrdinalIgnoreCase;
+							if (comparer.Equals(str, "1")) result = true;
+							else if (comparer.Equals(str, "0")) result = false;
+							else if (comparer.Equals(str, "t")) result = true;
+							else if (comparer.Equals(str, "f")) result = false;
+						}
+
+					}
+				}
+			}
+			// swallow any exception and return null
+			catch { }
+			return result;
+		}
+
+		#endregion
+
+		public static bool IsIntegral(this object o)
+		{
+			return (o is byte) || (o is sbyte) ||
+				(o is short) || (o is ushort) ||
+				(o is int) || (o is uint) ||
+				(o is long) || (o is ulong);
 		}
 
 		public static bool IsNotDefault<T>(this T instance)
