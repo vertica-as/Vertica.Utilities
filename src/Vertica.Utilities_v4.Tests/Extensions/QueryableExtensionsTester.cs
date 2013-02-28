0200b1785a13fbdee845d61cb57af65a4f1696d0
Daniel González García <danielgonnet@gmail.com>
2013-02-28 22:33:23 +0100

    added Queryable.Paginate()

---
+++ src/Vertica.Utilities_v4.Tests/Extensions/QueryableExtensionsTester.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Vertica.Utilities_v4.Collections;
+using Vertica.Utilities_v4.Extensions.QueryableExt;
+
+namespace Vertica.Utilities_v4.Tests.Extensions
+{
+	[TestFixture]
+	public class QueryableExtensionsTester
+	{
+		#region Paginate
+
+		[Test]
+		public void Paginate_NullInput_Exception()
+		{
+			IQueryable<int> nullQuery = null;
+			Assert.That(() => nullQuery.Paginate(new Pagination(1, 1)), Throws.InstanceOf<ArgumentNullException>());
+		}
+
+		[Test]
+		public void Paginate_EmptyInput_Empty()
+		{
+			IQueryable<int> emptyQuery = new int[0].AsQueryable();
+			Assert.That(emptyQuery.Paginate(new Pagination(1, 1)), Is.Empty);
+		}
+
+		[Test]
+		public void Paginate_WithinBounds_PageOfData()
+		{
+			IQueryable<int> oneToTen = Enumerable.Range(1, 10).AsQueryable();
+
+			Assert.That(oneToTen.Paginate(new Pagination(3, 2)), Is.EqualTo(new[] { 4, 5, 6 }));
+		}
+
+		[Test]
+		public void Paginate_OutsideBounds_Empty()
+		{
+			IQueryable<int> oneToTen = Enumerable.Range(1, 10).AsQueryable();
+
+			Assert.That(oneToTen.Paginate(new Pagination(5, 3)), Is.Empty);
+		}
+
+		#endregion
+	}
+}
---
+++ src/Vertica.Utilities_v4/Extensions/Queryable.Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Vertica.Utilities_v4.Collections;
+
+namespace Vertica.Utilities_v4.Extensions.QueryableExt
+{
+	public static class QueryableExtensions
+	{
+		public static IQueryable<T> Paginate<T>(this IQueryable<T> nonPaginated, Pagination page)
+		{
+			Guard.AgainstNullArgument("nonPaginated", nonPaginated);
+
+			return nonPaginated
+					.Skip((int)page.FirstRecord - 1)
+					.Take((int)page.PageSize);
+		}
+	}
+}
