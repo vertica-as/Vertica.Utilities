43c4bd9b2837d1a1fcd70d662b0de64e8a6f31a1
Brian Holmgård Kristensen <bhk@yourhost.dk>
2015-10-23 15:21:19 +0200

    Introducing Tree datastructure.

---
+++ src/Vertica.Utilities_v4.Tests/Collections/TreeTester.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Vertica.Utilities_v4.Collections;
+using Vertica.Utilities_v4.Extensions.EnumerableExt;
+
+namespace Vertica.Utilities_v4.Tests.Collections
+{
+	[TestFixture]
+	public class TreeTester
+	{
+		[Test]
+		public void Build_Category_Tree()
+		{
+			var categories = new[]
+			{
+				new Category { Id = 1},
+				new Category { Id = 2},
+				new Category { Id = 3, ParentId = 1 },
+				new Category { Id = 4, ParentId = 5 }
+			};
+
+			Tree<Category, int> tree = categories.ToTree(c => c.Id, (c, p) => c.ParentId.HasValue ? p.Value(c.ParentId.Value) : p.None);
+
+			Assert.That(tree.Count(), Is.EqualTo(2));
+			Assert.That(tree.ElementAt(0).Model, Is.SameAs(categories[0]));
+			Assert.That(tree.ElementAt(1).Model, Is.SameAs(categories[1]));
+
+			Assert.That(tree.ElementAt(0).Count(), Is.EqualTo(1));
+			Assert.That(tree.ElementAt(0).ElementAt(0).Model, Is.SameAs(categories[2]));
+
+			Assert.That(tree.Orphans().Count(), Is.EqualTo(1));
+			Assert.That(tree.Orphans().ElementAt(0), Is.EqualTo(categories[3]));
+
+			Assert.That(tree[1], Is.Not.Null);
+			Assert.That(tree[1].Model, Is.SameAs(categories[0]));
+			Assert.That(tree[4], Is.Null);
+
+			TreeNode<Category> node;
+			Assert.That(tree.TryGet(5, out node), Is.False);
+			Assert.That(tree.TryGet(3, out node), Is.True);
+			Assert.That(node.Model, Is.SameAs(categories[2]));
+		}
+
+		public class Category
+		{
+			public int Id { get; set; }
+			public int? ParentId { get; set; }
+		}
+
+		[Test]
+		public void Build_Family_Tree_Of_Men_With_Projection()
+		{
+			var members = new[]
+			{
+				new FamilyMember { Name = "Son", ParentName = "Dad"},
+				new FamilyMember { Name = "Grand Dad" },
+				new FamilyMember { Name = "Dad", ParentName = "Grand dad" }
+			};
+
+			Tree<FamilyMember, string, string> tree = members.ToTree(
+				x => x.Name, 
+				(x, p) => x.ParentName != null ? p.Value(x.ParentName) : p.None, 
+				x => x.Name, 
+				StringComparer.OrdinalIgnoreCase);
+
+			Assert.That(tree.Count(), Is.EqualTo(1));
+			Assert.That(tree.ElementAt(0).Model, Is.EqualTo("Grand Dad"));
+
+			Assert.That(tree.ElementAt(0).Count(), Is.EqualTo(1));
+			Assert.That(tree.ElementAt(0).ElementAt(0).Model, Is.EqualTo("Dad"));
+
+			Assert.That(tree.ElementAt(0).ElementAt(0).Count(), Is.EqualTo(1));
+			Assert.That(tree.ElementAt(0).ElementAt(0).ElementAt(0).Model, Is.EqualTo("Son"));
+			Assert.That(tree.ElementAt(0).ElementAt(0).ElementAt(0).Parent.Model, Is.EqualTo("Dad"));
+			Assert.That(tree.ElementAt(0).ElementAt(0).ElementAt(0).Parent.Parent.Model, Is.EqualTo("Grand Dad"));
+
+			CollectionAssert.AreEqual(tree["Grand Dad"].Breadcrumb(), new[] { "Grand Dad" });
+			CollectionAssert.AreEqual(tree["dad"].Breadcrumb(), new[] { "Grand Dad", "Dad" });
+			CollectionAssert.AreEqual(tree["Son"].Breadcrumb(), new[] {"Grand Dad", "Dad", "Son"});
+		}
+
+		public class FamilyMember
+		{
+			public string Name { get; set; }
+			public string ParentName { get; set; }
+		}
+	}
+}
---
+++ src/Vertica.Utilities_v4/Collections/Tree.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vertica.Utilities_v4.Collections
+{
+	public class Tree<TModel, TKey> : Tree<TModel, TModel, TKey>
+	{
+		internal protected Tree(IEnumerable<TModel> items, Func<TModel, TKey> key, Func<TModel, Parent, Parent.Key> parentKey, IEqualityComparer<TKey> comparer = null)
+			: base(items, key, parentKey, x => x, comparer)
+		{
+		}
+	}
+
+	public class Tree<TItem, TModel, TKey> : IEnumerable<TreeNode<TModel>>
+	{
+		private readonly Dictionary<TKey, Tuple<TKey, Parent.Key, TModel, List<TKey>>> _tree;
+		private readonly List<TKey> _root;
+		private readonly HashSet<TKey> _orphans;
+
+		internal protected Tree(IEnumerable<TItem> items, Func<TItem, TKey> key, Func<TItem, Parent, Parent.Key> parentKey, Func<TItem, TModel> projection, IEqualityComparer<TKey> comparer = null)
+		{
+			if (items == null) throw new ArgumentNullException("items");
+			if (key == null) throw new ArgumentNullException("key");
+			if (parentKey == null) throw new ArgumentNullException("parentKey");
+			if (projection == null) throw new ArgumentNullException("projection");
+
+			// Optimize by setting the initial count to the number of items (if item is List, Collection, Array)
+			_tree = new Dictionary<TKey, Tuple<TKey, Parent.Key, TModel, List<TKey>>>(TreeCapacityOr(items as ICollection<TModel>, 0), comparer);
+
+			_root = new List<TKey>();
+			_orphans = new HashSet<TKey>(comparer);
+
+			var parentHelper = new Parent();
+
+			// Initialize Tree with all items
+			foreach (TItem item in items)
+			{
+				TKey itemKey = key(item);
+				Parent.Key itemParentKey = parentKey(item, parentHelper);
+
+				// No way to optimize/guess the initial capacity of the list
+				_tree[itemKey] = Tuple.Create(itemKey, itemParentKey, projection(item), new List<TKey>());
+			}
+
+			// Relate children to their parents
+			foreach (Tuple<TKey, Parent.Key, TModel, List<TKey>> item in _tree.Values)
+			{
+				// Test if we have a parent
+				if (item.Item2 != null)
+				{
+					Tuple<TKey, Parent.Key, TModel, List<TKey>> parent;
+					if (_tree.TryGetValue(item.Item2.Value, out parent))
+					{
+						parent.Item4.Add(item.Item1);
+					}
+					else
+					{
+						_orphans.Add(item.Item1);
+					}
+				}
+				else
+				{
+					_root.Add(item.Item1);
+				}
+			}
+		}
+
+		private static int TreeCapacityOr(ICollection<TModel> collection, int defaultCapacity)
+		{
+			return collection != null ? collection.Count : defaultCapacity;
+		}
+
+		public TreeNode<TModel> this[TKey key]
+		{
+			get
+			{
+				Tuple<TKey, Parent.Key, TModel, List<TKey>> item;
+				if (!_orphans.Contains(key) && _tree.TryGetValue(key, out item))
+					return new TreeNode<TModel>(_tree[key].Item3, GetEnumerator(item.Item4), () => item.Item2 != null ? this[item.Item2.Value] : null);
+
+				return null;				
+			}
+		}
+
+		public bool TryGet(TKey key, out TreeNode<TModel> node)
+		{
+			node = this[key];
+
+			return node != null;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public IEnumerator<TreeNode<TModel>> GetEnumerator()
+		{
+			return GetEnumerator(_root);
+		}
+
+		public IEnumerable<TModel> Orphans()
+		{
+			return _orphans.Select(x => _tree[x].Item3);
+		}
+
+		private IEnumerator<TreeNode<TModel>> GetEnumerator(List<TKey> nodes)
+		{
+			if (nodes == null) throw new ArgumentNullException("nodes");
+
+			return nodes.Select(x => this[x]).Where(x => x != null).GetEnumerator();
+		}
+
+		public class Parent
+		{
+			public Key Value(TKey key)
+			{
+				return new Key(key);
+			}
+
+			public Key None { get { return null; } }
+
+			public class Key
+			{
+				internal Key(TKey key)
+				{
+					Value = key;
+				}
+
+				internal TKey Value { get; private set; }
+			}
+		}
+	}
+
+	public class TreeNode<TModel> : IEnumerable<TreeNode<TModel>>
+	{
+		private readonly IEnumerator<TreeNode<TModel>> _children;
+		private readonly Func<TreeNode<TModel>> _parent;
+
+		internal TreeNode(TModel model, IEnumerator<TreeNode<TModel>> children, Func<TreeNode<TModel>> parent)
+		{
+			Model = model;
+			_children = children;
+			_parent = parent;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public IEnumerator<TreeNode<TModel>> GetEnumerator()
+		{
+			return _children;
+		}
+
+		public TreeNode<TModel> Parent
+		{
+			get { return _parent(); }
+		}
+
+		public TModel Model { get; private set; }
+
+		public TModel[] Breadcrumb()
+		{
+			var stack = new Stack<TModel>();
+
+			TreeNode<TModel> current = this;
+
+			while (current != null)
+			{
+				stack.Push(current.Model);
+				current = current.Parent;
+			}
+
+			return stack.ToArray(); 
+		}
+	}
+}
