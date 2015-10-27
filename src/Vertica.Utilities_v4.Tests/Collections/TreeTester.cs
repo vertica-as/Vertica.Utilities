f0510c79f11fa884ba2e95d82a76324f4fef2c3e
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-10-27 07:59:36 +0100

    split category tree test into several

--- src/Vertica.Utilities_v4.Tests/Collections/TreeTester.cs
+++ src/Vertica.Utilities_v4.Tests/Collections/TreeTester.cs
@@ -10,16 +10,19 @@ namespace Vertica.Utilities_v4.Tests.Collections
 	[TestFixture]
 	public class TreeTester
 	{
+		private static readonly Category c1 = new Category { Id = 1 },
+			c2 = new Category { Id = 2 },
+			c3 = new Category { Id = 3, ParentId = 1 },
+			c4 = new Category { Id = 4, ParentId = 5 };
+
 		[Test]
-		public void Build_Category_Tree()
+		public void ToTree_BuildsATree_OfWrappedCategories()
 		{
-			Category c1 = new Category { Id = 1},
-				c2 = new Category { Id = 2},
-				c3 = new Category { Id = 3, ParentId = 1 },
-				c4 = new Category { Id = 4, ParentId = 5 };
 			var categories = new[] { c1, c2, c3, c4 };
 
-			Tree<Category, int> tree = categories.ToTree(c => c.Id, (c, p) => c.ParentId.HasValue ? p.Value(c.ParentId.Value) : p.None);
+			Tree<Category, int> tree = categories.ToTree(
+				c => c.Id,
+				(c, p) => c.ParentId.HasValue ? p.Value(c.ParentId.Value) : p.None);
 
 			Assert.That(tree.Count(), Is.EqualTo(2));
 			Assert.That(tree.ElementAt(0).Model, Is.SameAs(c1));
@@ -27,18 +30,71 @@ namespace Vertica.Utilities_v4.Tests.Collections
 
 			Assert.That(tree.ElementAt(0).Count(), Is.EqualTo(1));
 			Assert.That(tree.ElementAt(0).ElementAt(0).Model, Is.SameAs(c3));
+		}
+
+		[Test]
+		public void Orphans_NodesWithoutParent_GoToAnotherStructure()
+		{
+			var categories = new[] { c1, c2, c3, c4 };
+
+			Tree<Category, int> tree = categories.ToTree(
+				c => c.Id, 
+				(c, p) => c.ParentId.HasValue ? p.Value(c.ParentId.Value) : p.None);
 
 			Assert.That(tree.Orphans().Count(), Is.EqualTo(1));
 			Assert.That(tree.Orphans().ElementAt(0), Is.EqualTo(c4));
+		}
+
+		[Test]
+		public void Indexer_NodeKeysInTree_NodeReference()
+		{
+			var categories = new[] { c1, c2, c3, c4 };
+
+			Tree<Category, int> tree = categories.ToTree(
+				c => c.Id,
+				(c, p) => c.ParentId.HasValue ? p.Value(c.ParentId.Value) : p.None);
 
 			Assert.That(tree[1], Is.Not.Null);
 			Assert.That(tree[1].Model, Is.SameAs(c1));
-			Assert.That(tree[4], Is.Null);
+		}
+
+		[Test]
+		public void Indexer_NodeKeysNotInTree_Null()
+		{
+			var categories = new[] { c1, c2, c3, c4 };
+
+			Tree<Category, int> tree = categories.ToTree(c => c.Id, (c, p) => c.ParentId.HasValue ? p.Value(c.ParentId.Value) : p.None);
+
+			Assert.That(tree[4], Is.Null, "c4 is an orphan");
+			Assert.That(tree[42], Is.Null, "c42 does not even exist");
+		}
+
+		[Test]
+		public void TryGet_NodeKeysInTree_TrueAndNodeReference()
+		{
+			var categories = new[] { c1, c2, c3, c4 };
+
+			Tree<Category, int> tree = categories.ToTree(
+				c => c.Id,
+				(c, p) => c.ParentId.HasValue ? p.Value(c.ParentId.Value) : p.None);
 
 			TreeNode<Category> node;
-			Assert.That(tree.TryGet(5, out node), Is.False);
 			Assert.That(tree.TryGet(3, out node), Is.True);
 			Assert.That(node.Model, Is.SameAs(c3));
+		}
+
+		[Test]
+		public void TryGet_NodeKeysNotInTree_FalseAndNull()
+		{
+			var categories = new[] { c1, c2, c3, c4 };
+
+			Tree<Category, int> tree = categories.ToTree(
+				c => c.Id,
+				(c, p) => c.ParentId.HasValue ? p.Value(c.ParentId.Value) : p.None);
+
+			TreeNode<Category> node;
+			Assert.That(tree.TryGet(4, out node), Is.False, "c4 is an orphan");
+			Assert.That(tree.TryGet(42, out node), Is.False, "c42 does not even exist");
 		}
 
 		[Test]
