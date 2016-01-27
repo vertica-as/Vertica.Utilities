f186289315a840d53b0a4b57a596b9e722a71710
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2016-01-27 09:39:54 +0100

    added PaginatedResults and split Pagination to its own file

---
+++ src/Vertica.Utilities_v4.Tests/Collections/PaginatedResultsTester.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using Vertica.Utilities_v4.Collections;
+
+namespace Vertica.Utilities_v4.Tests.Collections
+{
+	[TestFixture]
+	public class PaginatedResultsTester
+	{
+		[Test]
+		public void Records_FirstPage_OneToPageSize()
+		{
+			var pagination = new Pagination(3, 1);
+			var subject = new PaginatedResults<int>(new[] { 10, 20, 30 }, 7, pagination);
+
+			Assert.That(subject.RecordNumbers.LowerBound, Is.EqualTo(1));
+			Assert.That(subject.RecordNumbers.UpperBound, Is.EqualTo(3));
+		}
+
+		[Test]
+		public void Records_CompletePage_FirstRecordPlusPageSize()
+		{
+			var pagination = new Pagination(3, 2);
+			var subject = new PaginatedResults<int>(new[] { 40, 50, 60 }, 7, pagination);
+
+			Assert.That(subject.RecordNumbers.LowerBound, Is.EqualTo(4));
+			Assert.That(subject.RecordNumbers.UpperBound, Is.EqualTo(6));
+		}
+
+		[Test]
+		public void Records_IncompletePage_FirstRecordPlusResultsLength()
+		{
+			var pagination = new Pagination(3, 3);
+			var subject = new PaginatedResults<int>(new[] { 70 }, 7, pagination);
+
+			Assert.That(subject.RecordNumbers.LowerBound, Is.EqualTo(7));
+			Assert.That(subject.RecordNumbers.UpperBound, Is.EqualTo(7));
+
+			subject = new PaginatedResults<int>(new[] { 70, 80 }, 8, pagination);
+
+			Assert.That(subject.RecordNumbers.LowerBound, Is.EqualTo(7));
+			Assert.That(subject.RecordNumbers.UpperBound, Is.EqualTo(8));
+		}
+
+		[Test]
+		public void Records_LessThanOnePage_OneToLength()
+		{
+			var pagination = new Pagination(3, 1);
+			var subject = new PaginatedResults<int>(new[] { 10 }, 1, pagination);
+
+			Assert.That(subject.RecordNumbers.LowerBound, Is.EqualTo(1));
+			Assert.That(subject.RecordNumbers.UpperBound, Is.EqualTo(1));
+
+			subject = new PaginatedResults<int>(new[] { 10, 20 }, 2, pagination);
+
+			Assert.That(subject.RecordNumbers.LowerBound, Is.EqualTo(1));
+			Assert.That(subject.RecordNumbers.UpperBound, Is.EqualTo(2));
+		}
+	}
+}
--- src/Vertica.Utilities_v4/Collections/PaginatedCollection.cs
+++ src/Vertica.Utilities_v4/Collections/PaginatedCollection.cs
@@ -5,58 +5,6 @@ using System.Linq;
 
 namespace Vertica.Utilities_v4.Collections
 {
-	public interface IPaginatedCollection<out T> : IEnumerable<T>
-	{
-		uint CurrentPage { get; }
-		uint Pagesize { get; }
-		uint NumberOfPages { get; }
-		IEnumerable<T> Collection { get; }
-	}
-
-	[Serializable]
-	public struct Pagination
-	{
-		public Pagination(uint pageSize, uint pageNumber) : this()
-		{
-			PageSize = pageSize;
-			PageNumber = pageNumber;
-		}
-
-		public uint PageNumber { get; private set; }
-		public uint PageSize { get; private set; }
-
-		public uint FirstRecord
-		{
-			get
-			{
-				return (PageNumber == 0) ?
-					1 :
-					((PageNumber - 1) * PageSize) + 1;
-			}
-		}
-
-		public uint LastRecord
-		{
-			get
-			{
-				return (PageSize == 0) ?
-					FirstRecord :
-					FirstRecord + PageSize - 1;
-			}
-		}
-
-		public uint PageCount(uint totalCount)
-		{
-			uint n = totalCount % PageSize;
-
-			uint numberOfPages = n > 0 ?
-				(totalCount / PageSize) + 1 :
-				totalCount / PageSize;
-
-			return numberOfPages;
-		}
-	}
-
 	[Serializable]
 	public class PaginatedCollection<T> : IPaginatedCollection<T>
 	{
