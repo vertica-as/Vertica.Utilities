23437ad68149fafb7fe82ad4855ddbddf51dcafb
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2016-11-03 15:49:38 +0100

    Deperecate Control extensions
    Webforms should be on their way out and UI composition is not done this way in MVC.

--- src/Vertica.Utilities_v4/Extensions/Control.Extensions.cs
+++ src/Vertica.Utilities_v4/Extensions/Control.Extensions.cs
@@ -6,78 +6,84 @@ using Vertica.Utilities_v4.Resources;
 
 namespace Vertica.Utilities_v4.Extensions.ControlExt
 {
-  public static class ControlExtensions
-  {
-	  public static T FindControl<T>(this Control parentControl, string targetControlID)
+	[Obsolete(".NET Standard")]
+	public static class ControlExtensions
+	{
+		[Obsolete(".NET Standard")]
+		public static T FindControl<T>(this Control parentControl, string targetControlID)
 			where T : Control
-	  {
-		  Guard.AgainstNullArgument("parentControl", parentControl);
-		  Guard.AgainstArgument("targetControlID", targetControlID.IsEmpty());
+		{
+			Guard.AgainstNullArgument("parentControl", parentControl);
+			Guard.AgainstArgument("targetControlID", targetControlID.IsEmpty());
 
-		  Control targetControlBase = parentControl.FindControl(targetControlID);
+			Control targetControlBase = parentControl.FindControl(targetControlID);
 
-		  if (targetControlBase == null)
-		  {
-			  ExceptionHelper.Throw<ArgumentOutOfRangeException>(Exceptions.ControlExtensions_NotFoundTemplate,
-				  targetControlID,
-				  parentControl.ID);
-		  }
+			if (targetControlBase == null)
+			{
+				ExceptionHelper.Throw<ArgumentOutOfRangeException>(Exceptions.ControlExtensions_NotFoundTemplate,
+					targetControlID,
+					parentControl.ID);
+			}
 
-		  return (T)targetControlBase;
-	  }
+			return (T)targetControlBase;
+		}
 
-	  public static bool TryFindControl<T>(this Control parentControl, string targetControlID, out T control)
-			where T : Control
-	  {
-		  control = default(T);
+		[Obsolete(".NET Standard")]
+		public static bool TryFindControl<T>(this Control parentControl, string targetControlID, out T control)
+			  where T : Control
+		{
+			control = default(T);
 
-		  try
-		  {
-			  control = FindControl<T>(parentControl, targetControlID);
+			try
+			{
+				control = FindControl<T>(parentControl, targetControlID);
 
-			  return control != null;
-		  }
-		  catch (ArgumentException)
-		  { }
-		  catch (InvalidCastException)
-		  { }
+				return control != null;
+			}
+			catch (ArgumentException)
+			{ }
+			catch (InvalidCastException)
+			{ }
 
-		  return false;
-	  }
+			return false;
+		}
 
-	  public static bool SelectValue(this ListControl list, string value)
-	  {
-		  if (list == null) throw new ArgumentNullException("list");
+		[Obsolete(".NET Standard")]
+		public static bool SelectValue(this ListControl list, string value)
+		{
+			if (list == null) throw new ArgumentNullException("list");
 
-		  if (list.Items.Count > 0)
-		  {
-			  ListItem listItem = list.Items.FindByValue(value);
+			if (list.Items.Count > 0)
+			{
+				ListItem listItem = list.Items.FindByValue(value);
 
-			  if (listItem != null)
-			  {
-				  list.ClearSelection();
-				  listItem.Selected = true;
-				  return true;
-			  }
-		  }
+				if (listItem != null)
+				{
+					list.ClearSelection();
+					listItem.Selected = true;
+					return true;
+				}
+			}
 
-		  return false;
-	  }
+			return false;
+		}
 
-	  private static string userControlFileName<T>() where T : UserControl
-	  {
-		  return typeof(T).Name + ".ascx";
-	  }
+		private static string userControlFileName<T>() where T : UserControl
+		{
+			return typeof(T).Name + ".ascx";
+		}
 
-	  public static T LoadControl<T>(this TemplateControl entry) where T : UserControl
-	  {
-		  return (T)entry.LoadControl(userControlFileName<T>());
-	  }
+		[Obsolete(".NET Standard")]
+		public static T LoadControl<T>(this TemplateControl entry) where T : UserControl
+		{
+			return (T)entry.LoadControl(userControlFileName<T>());
+		}
 
-	  public static T LoadControl<T>(this TemplateControl entry, Uri userControlsPath) where T : UserControl
-	  {
-		  return (T)entry.LoadControl(userControlsPath.ToString().IfNotThere().Append("/") + userControlFileName<T>());
-	  }
+		[Obsolete(".NET Standard")]
+		public static T LoadControl<T>(this TemplateControl entry, Uri userControlsPath) where T : UserControl
+		{
+			return (T)entry.LoadControl(userControlsPath.ToString().IfNotThere().Append("/") + userControlFileName<T>());
+		}
 
-  }
+	}
 }
