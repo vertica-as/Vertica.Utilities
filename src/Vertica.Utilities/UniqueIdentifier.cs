7b8c40c7ae826f303dfd6679d37b1104ab6f807d
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-05-08 13:29:58 +0200

    remove obsolete types

--- src/Vertica.Utilities/UniqueIdentifier.cs
+++ src/Vertica.Utilities/UniqueIdentifier.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Runtime.InteropServices;
 
 namespace Vertica.Utilities_v4
 {
@@ -33,49 +31,5 @@ namespace Vertica.Utilities_v4
 
 			return new Guid(guidArray);
 		}
-
-		[DllImport("rpcrt4.dll", SetLastError = true)]
-		private static extern int UuidCreateSequential(out Guid guid); 
-		private const int RPC_S_OK = 0;
-
-        // Used to sort the bytes of the Guid according to Alberto Ferrari article (http://sqlblog.com/blogs/alberto_ferrari/archive/2007/08/31/how-are-guids-sorted-by-sql-server.aspx)
-        private static readonly int[] _sqlOrderMap = new[] {3, 2, 1, 0, 5, 4, 7, 6, 9, 8, 15, 14, 13, 12, 11, 10};
-
-		[Obsolete(".NET Standard")]
-		public static Guid Sequential()
-		{
-			Guid guid = generateSequentialGuid();
-			return setSortOrder(guid); 
-		}
-
-		private static Guid generateSequentialGuid()
-		{
-			Guid sequentialguid;
-
-			int hr = UuidCreateSequential(out sequentialguid);
-
-			if (hr != RPC_S_OK)
-			{
-				throw new Win32Exception(hr, string.Format("UuidCreateSequential() call failed: {0}", hr));
-			}
-
-			return sequentialguid;
-		}
-
-		private static Guid setSortOrder(Guid guid)
-		{
-			byte[] bytes = guid.ToByteArray();
-			var copyBytes = new byte[16];
-
-			bytes.CopyTo(copyBytes, 0);
-
-			for (int mapIndex = 0; mapIndex < 10; mapIndex++)
-			{
-				bytes[mapIndex] = copyBytes[_sqlOrderMap[mapIndex]];
-			}
-
-			guid = new Guid(bytes);
-			return guid;
-		} 
 	}
 }
