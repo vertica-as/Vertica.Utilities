b71b0a41f6c368509ad3eb8891f02655552a32c7
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-05-24 09:09:42 +0200

    fix typo in variable declaration

--- build/Upload-Test-Results.ps1
+++ build/Upload-Test-Results.ps1
@@ -1,3 +1,3 @@
 $wc = New-Object 'System.Net.WebClient'
-$test-results = Resolve-Path .\src\Vertica.Utilities.Tests\TestResults\test-results.xml
+$test_results = Resolve-Path .\src\Vertica.Utilities.Tests\TestResults\test-results.xml
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", $test_results)
